792329d6bfa021aafb3521ab5e8cc7b470a782f5
flaviopazos <flavio.pazos@gmail.com>
2024-08-26 17:30:23 -0300

    Conflicto

--- the_best_plot.R
+++ the_best_plot.R
@@ -3,5 +3,5 @@ library(ggplot2)
 
 # Aca hago un grafico
 
-ggplot(data = ToothGrowth, aes( x = supp, y = len, fill = dose) ) +
+ggplot(data = ToothGrowth, aes( x = supp, y = len, fill = dodge) ) +
   geom_col(position = "dodge")
